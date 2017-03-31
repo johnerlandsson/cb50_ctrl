f355ec988b9d496f0bef0657738e83d5d26093ad
John Erlandsson <john@lunatech.se>
2017-03-31 15:10:43 +0200

    Read pd done

--- www/assets/js/recipe.js
+++ www/assets/js/recipe.js
@@ -1,15 +1,30 @@
 //The module
 angular.module("RecipeApp", ['ui-notification']).controller("RecipeCtrl", function($scope, $http, Notification) {
-  $scope.pump = false;
+	$scope.pd = {};
+
+  function get_pd() {
+    $http.get("get_pd").then(function successCallback(response) {
+			$scope.pd = response.data;
+    }, function errorCallback(response) {
+      Notification.error({
+        message: "Failed to receive process data from server."
+      });
+    });
+  }
+	function update_pd() {
+		get_pd();
+	}
+
+	window.setInterval(update_pd, 500);
 
   $scope.pump_btn_clicked = function() {
-    console.log("pump");
+		console.log($scope.pd["pump"]);
   }
   $scope.run_btn_clicked = function() {
-    console.log("run");
+		console.log($scope.pd["run_recipe"]);
   }
   $scope.mixer_btn_clicked = function() {
-    console.log("mixer");
+		console.log($scope.pd["mixer"]);
   }
 }).config(function(NotificationProvider) { //Configure notifications
   NotificationProvider.setOptions({
