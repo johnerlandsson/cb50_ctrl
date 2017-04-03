31a485c64392eab13bb21bac5420000fad1c6b3d
John Erlandsson <john@lunatech.se>
2017-04-03 10:45:14 +0200

    Toolbar working

--- www/assets/js/recipe.js
+++ www/assets/js/recipe.js
@@ -22,7 +22,7 @@ angular.module("RecipeApp", ['ui-notification']).controller("RecipeCtrl", functi
   function get_pd() {
     $http.get("get_pd").then(function successCallback(response) {
       $scope.pd = response.data;
-      console.log($scope.pd);
+      //console.log($scope.pd);
     }, function errorCallback(response) {
       Notification.error({
         message: "Failed to receive process data from server."
@@ -32,17 +32,47 @@ angular.module("RecipeApp", ['ui-notification']).controller("RecipeCtrl", functi
 
   function update_pd() {
     get_pd();
+
+		// Set button colors
+    if ($scope.pd["pump"]) {
+      $scope.pump_btn_style = {
+        "background-color": "rgb(102, 102, 102)"
+      };
+    } else {
+      $scope.pump_btn_style = {
+        "background-color": "rgb(153, 153, 153)"
+      };
+    }
+    if ($scope.pd["mixer"]) {
+      $scope.mixer_btn_style = {
+        "background-color": "rgb(102, 102, 102)"
+      };
+    } else {
+      $scope.mixer_btn_style = {
+        "background-color": "rgb(153, 153, 153)"
+      };
+    }
+    if ($scope.pd["run_recipe"]) {
+      $scope.run_btn_style = {
+        "background-color": "rgb(102, 102, 102)"
+      };
+    } else {
+      $scope.run_btn_style = {
+        "background-color": "rgb(153, 153, 153)"
+      };
+    }
   }
 
   $scope.intervalID = window.setInterval(update_pd, 500);
 
-	// Pump button clicked
+  // Pump button clicked
   $scope.pump_btn_clicked = function() {
     window.clearInterval($scope.intervalID);
     $scope.pd["pump"] = !$scope.pd["pump"];
     $http.post('put_pd', {
       message: $scope.pd
     }).then(function successCallback(response) {
+      $scope.pd = response.data;
       $scope.intervalID = window.setInterval(update_pd, 500);
     }, function errorCallback(response) {
       Notification.error({
@@ -50,15 +80,37 @@ angular.module("RecipeApp", ['ui-notification']).controller("RecipeCtrl", functi
       });
     });
   }
-	
-	// Run recipe button clicked
+
+  // Run recipe button clicked
   $scope.run_btn_clicked = function() {
-    console.log($scope.pd["run_recipe"]);
+    window.clearInterval($scope.intervalID);
+    $scope.pd["run_recipe"] = !$scope.pd["run_recipe"];
+    $http.post('put_pd', {
+      message: $scope.pd
+    }).then(function successCallback(response) {
+      $scope.pd = response.data;
+      $scope.intervalID = window.setInterval(update_pd, 500);
+    }, function errorCallback(response) {
+      Notification.error({
+        message: "Failed send run recipe value"
+      });
+    });
   }
 
-	// Mixer button clicked
+  // Mixer button clicked
   $scope.mixer_btn_clicked = function() {
-    console.log($scope.pd["mixer"]);
+    window.clearInterval($scope.intervalID);
+    $scope.pd["mixer"] = !$scope.pd["mixer"];
+    $http.post('put_pd', {
+      message: $scope.pd
+    }).then(function successCallback(response) {
+      $scope.pd = response.data;
+      $scope.intervalID = window.setInterval(update_pd, 500);
+    }, function errorCallback(response) {
+      Notification.error({
+        message: "Failed send mixer value"
+      });
+    });
   }
 }).config(function(NotificationProvider) { //Configure notifications
   NotificationProvider.setOptions({
